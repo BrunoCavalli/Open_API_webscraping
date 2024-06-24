46b2c250b1158bfdb4527d7510ca5dbec72ad2e4
BrunoCavalli <87094109+BrunoCavalli@users.noreply.github.com>
2024-06-24 19:35:21 -0300

    implementação funcionando
    falta pensar em melhores maneiras de usar as análises da API

--- openAPI.py
+++ openAPI.py
@@ -3,40 +3,23 @@
 import DoctorAlia
 import pandas as pd
 from openai import OpenAI
-client = OpenAI(api_key="API KEY")
+client = OpenAI(api_key="API-KEY")
 
-file = client.files.create(
-  file=open("dados.csv", "rb"),
-  purpose='assistants'
-)
 
-"""
+
+
 dados = pd.read_csv('./dados.csv')
 #print(dados)
 dados_str = dados.to_string()
-"""
 
-assistant = client.beta.assistants.create(
-  instructions="Your task is to make coments about the data in the csv file that you will receive",
-  model="gpt-3.5-turbo",
-  tools=[{"type": "code_interpreter"}],
-  tool_resources={
-    "code_interpreter": {
-      "file_ids": [file.id]
-    }
-  }
-)
 
-responce= assistant.instructions[0]
-print(responce)
 
-"""
 response = client.chat.completions.create(
  model="gpt-3.5-turbo",
  messages=[
    {
      "role": "assistant",
-     "content": "You will be provided with an csv file, and your task is to make an analyzes of it."
+     "content": "You will be provided with an csv file, and your task is to make comentaries of it."
    },
    {
         "role": "user",
@@ -44,10 +27,10 @@ response = client.chat.completions.create(
    },
  ],
  temperature=0.7,
- max_tokens=64,
+ max_tokens=1000,
  top_p=1
 )
 
 
-resposta = response['choices'][0]['message']['content']
-"""
+resposta = response.choices[0].message.content
+print(resposta)
