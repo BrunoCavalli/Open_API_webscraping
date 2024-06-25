534cf18bda25552419dc5e274f72af70d8dbcb43
LuisaQuerido <161589320+LuisaQuerido@users.noreply.github.com>
2024-06-25 09:48:21 -0300

    Update limpeza.py
    Adicionei e atualizei um comentario

--- limpeza.py
+++ limpeza.py
@@ -1,5 +1,7 @@
+""" Modulo de preparacao e limpeza de dados """
+
 import re
 def ajeita_negocio(str):
     return re.sub(r'[^0-9]', '', str)
 
-#este pequeno módulo limpa os dados 
+# Este pequeno módulo limpa os dados 
