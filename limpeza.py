0156648d20e7fb4c99bfb450e2c0cfe1e79adbd3
samdreis <162479646+samdreis@users.noreply.github.com>
2024-06-25 11:28:40 -0300

    Update limpeza.py

--- limpeza.py
+++ limpeza.py
@@ -1,7 +1,12 @@
 """ Modulo de preparacao e limpeza de dados """
 
 import re
+
 def ajeita_negocio(str):
     return re.sub(r'[^0-9]', '', str)
+
+def arruma_negocio(str):
+    palavra = "opiniões"
+    return str.replace(palavra, f" {palavra}")
 
 # Este pequeno módulo limpa os dados 
