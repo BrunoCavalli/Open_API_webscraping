bfa8fa20e5f6e9f5abfd5465ce50c7f9b959248a
LuisaQuerido <161589320+LuisaQuerido@users.noreply.github.com>
2024-06-25 22:34:06 -0300

    Atualizando o código

--- tirar comentarios.py
+++ tirar comentarios.py
@@ -6,8 +6,7 @@ dados_path2 = './review.csv'
 
 # perguntas da tabela review
 perguntas_review = [
-    "O comentário é positivo ou negativo?",
-    "Qual é a média inteira das estrelas do doutor?"
+    "O comentário é positivo ou negativo?"
 ]
 
 resultado = analyze_data(dados_path2, perguntas_review)
