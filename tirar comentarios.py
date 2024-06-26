bf6aa9edf04ed349bd51c81c45564764ca09bd57
BrunoCavalli <87094109+BrunoCavalli@users.noreply.github.com>
2024-06-25 22:16:28 -0300

    Update tirar comentarios.py

--- tirar comentarios.py
+++ tirar comentarios.py
@@ -1,8 +1,13 @@
+""" Modulo para tirar os comentarios """
+
 from openAPI import analyze_data
 
 dados_path2 = './review.csv'
+
+# perguntas da tabela review
 perguntas_review = [
-    "O comentário é positivo ou negativo?"
+    "O comentário é positivo ou negativo?",
+    "Qual é a média inteira das estrelas do doutor?"
 ]
 
 resultado = analyze_data(dados_path2, perguntas_review)
