8757707bf765de774a49dcf85d3cb39c42951d05
LuisaQuerido <161589320+LuisaQuerido@users.noreply.github.com>
2024-06-25 22:32:27 -0300

    Atualizando os comentários

--- openAPI.py
+++ openAPI.py
@@ -1,11 +1,14 @@
+""" Modulo do OpenAPI """
+
 import pandas as pd
 from openai import OpenAI
 import concurrent.futures
 
-# Inicializar o cliente OpenAI com a chave da API
+# inicializar o cliente OpenAI com a chave da API
 chave_api = input("digite sua chave aqui:")
 client = OpenAI(api_key=chave_api)
 
+# define o modelo para usar o chatGPT
 def get_response(prompt):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-0125",
@@ -15,7 +18,8 @@ def get_response(prompt):
         top_p=1
     )
     return response.choices[0].message.content
-    
+
+# define o prompt e retorna o modelo
 def process_row(row, pergunta):
     prompt = [
         {"role": "assistant", "content": "You will receive a CSV row, your task is to make comments about the content of it."},
@@ -24,16 +28,17 @@ def process_row(row, pergunta):
     ]
     return get_response(prompt)
 
+
 def analyze_data(dados_path, perguntas):
-    # Ler o arquivo CSV
+    # ler o arquivo CSV
     dados = pd.read_csv(dados_path)
     
-    # Iterar sobre a lista de perguntas
+    # iterar sobre a lista de perguntas
     for idx, pergunta in enumerate(perguntas):
         coluna_nome = f'{pergunta}'
         dados[coluna_nome] = ""
         
-        # Using ThreadPoolExecutor for parallel API calls
+        # usar ThreadPoolExecutor para chamada de APIs paralelas
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = {executor.submit(process_row, dados.iloc[i], pergunta): i for i in range(len(dados))}
             for future in concurrent.futures.as_completed(futures):
@@ -41,21 +46,21 @@ def analyze_data(dados_path, perguntas):
                 resposta = future.result()
                 dados.at[idl, coluna_nome] = resposta
     
-    # Salvar o DataFrame modificado de volta em um arquivo CSV
+    # salvar o DataFrame modificado de volta em um arquivo CSV
     dados.to_csv(dados_path, index=False)
     
     return dados
 
 def analyze_review(dados_path, perguntas):
-    # Ler o arquivo CSV
+    # ler o arquivo CSV
     review = pd.read_csv(dados_path)
     
-    # Iterar sobre a lista de perguntas
+    # iterar sobre a lista de perguntas
     for idx, pergunta in enumerate(perguntas):
         coluna_nome = f'{pergunta}'
         review[coluna_nome] = ""
         
-        # Using ThreadPoolExecutor for parallel API calls
+        # usar ThreadPoolExecutor para chamada de APIs paralelas
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = {executor.submit(process_row, review.iloc[i], pergunta): i for i in range(len(review))}
             for future in concurrent.futures.as_completed(futures):
@@ -63,7 +68,7 @@ def analyze_review(dados_path, perguntas):
                 resposta = future.result()
                 review.at[idl, coluna_nome] = resposta
     
-    # Salvar o DataFrame modificado de volta em um arquivo CSV
+    # salvar o DataFrame modificado de volta em um arquivo CSV
     review.to_csv(dados_path, index=False)
     
     return review
