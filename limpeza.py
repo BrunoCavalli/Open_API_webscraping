43b914aca7223a53b799916ff1fae73c62a9ba27
LuisaQuerido <161589320+LuisaQuerido@users.noreply.github.com>
2024-06-25 22:26:30 -0300

    Atualizando os comentários

--- limpeza.py
+++ limpeza.py
@@ -2,15 +2,15 @@
 
 import re
 
-# este pequeno módulo irá limpar os dados
+# este pequeno modulo ira limpar os dados
 
 def ajeita_negocio(str):
     return re.sub(r'[^0-9]', '', str)
 
-# essa função irá manter apenas os caracteres númericos da string
+# essa função ira manter apenas os caracteres numericos da string
 
 def arruma_negocio(str):
     palavra = "opiniões"
     return str.replace(palavra, f" {palavra}")
 
-# essa função irá criar um espaço na frente da palavra 'opiniao'
+# essa função ira criar um espaço na frente da palavra 'opiniao'
